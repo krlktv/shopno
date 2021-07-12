d1ba9c852e964241d4cfa25c00e3560659f31122
Kirill <krlktv@mail.ru>
2021-07-12 15:29:35 +0300

    fix: minify js

--- src/js/script.js
+++ src/js/script.js
@@ -1,49 +1,46 @@
 'use strict';
 
-window.addEventListener('DOMContentLoaded', () => {
+// Popup
 
-	// Popup
+const popupTrigger = document.querySelectorAll('.works__img'),
+	popup = document.querySelector('.popup');
 
-	const popupTrigger = document.querySelectorAll('.works__img'),
-		popup = document.querySelector('.popup');
-
-	popupTrigger.forEach((img, index) => {
-		img.addEventListener('click', (img) => {
-			document.querySelector('.popup__img').src = img.target.src;
-		});
-		img.addEventListener('click', openPopup);
+popupTrigger.forEach((img, index) => {
+	img.addEventListener('click', (img) => {
+		document.querySelector('.popup__img').src = img.target.src;
 	});
+	img.addEventListener('click', openPopup);
+});
 
-	function closePopup() {
-		popup.classList.add('popup_hide');
-		popup.classList.remove('popup_show');
-		document.body.style.overflow = '';
+function closePopup() {
+	popup.classList.add('popup_hide');
+	popup.classList.remove('popup_show');
+	document.body.style.overflow = '';
+}
+
+function openPopup() {
+	popup.classList.add('popup_show');
+	popup.classList.remove('popup_hide');
+	document.body.style.overflow = 'hidden';
+}
+
+popup.addEventListener('click', (e) => {
+	if (e.target === popup) {
+		closePopup();
 	}
+});
 
-	function openPopup() {
-		popup.classList.add('popup_show');
-		popup.classList.remove('popup_hide');
-		document.body.style.overflow = 'hidden';
+document.addEventListener('keydown', (e) => {
+	if (e.code === "Escape" && popup.classList.contains('popup_show')) {
+		closePopup();
 	}
+});
 
-	popup.addEventListener('click', (e) => {
-		if (e.target === popup) {
-			closePopup();
-		}
-	});
+// Form reset
 
-	document.addEventListener('keydown', (e) => {
-		if (e.code === "Escape" && popup.classList.contains('popup_show')) {
-			closePopup();
-		}
-	});
+const form = document.querySelector('#form');
 
-	// Form reset
-
-	const form = document.querySelector('#form');
-
-	form.addEventListener('submit', function (e) {
-		e.preventDefault();
-		form.reset();
-	});
+form.addEventListener('submit', function (e) {
+	e.preventDefault();
+	form.reset();
 });
